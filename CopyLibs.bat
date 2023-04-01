881425142c1bb6c98c3e46e696f8ca342353c3f7
Luis Ricardo Alves Santos <luisricardo2825@gmail.com>
2023-04-01 10:45:26 -0300

    Remoção de libs desnecessárias

---
+++ CopyLibs.bat
@@ -0,0 +1,23 @@
+copy G:\Bibliotecas\mge-modelcore-4.10b62.jar G:\Libs
+copy G:\Bibliotecas\jape.jar G:\Libs
+copy G:\Bibliotecas\sanws.jar G:\Libs
+copy G:\Bibliotecas\json-20140107.jar G:\Libs
+copy G:\Bibliotecas\gson-2.1.jar G:\Libs
+copy G:\Bibliotecas\sanutil.jar G:\Libs
+copy G:\Bibliotecas\cuckoo.jar G:\Libs
+copy G:\Bibliotecas\SankhyaW-extensions.jar G:\Libs
+copy G:\Bibliotecas\SankhyaW-extensions.jar G:\Libs
+
+G:\Bibliotecas\mge-modelcore-4.10b62.jar
+G:\Bibliotecas\jape.jar
+G:\Bibliotecas\sanws.jar
+G:\Bibliotecas\json-20140107.jar
+G:\Bibliotecas\gson-2.1.jar
+G:\Bibliotecas\sanutil.jar
+G:\Bibliotecas\cuckoo.jar
+G:\Bibliotecas\SankhyaW-extensions.jar
+G:\Bibliotecas\SankhyaW-extensions.jar
+G:\Bibliotecas\javax.servlet.jar
+G:\Bibliotecas\commons-fileupload-1.2.2.jar
+G:\Bibliotecas\ojdbc7_12c.jar
+G:\Bibliotecas\joda-time-2.3.jar
